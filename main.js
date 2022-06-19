93f64902a45055b032b37b3e89532b0336d2f1b6
pascualSilvestri <pascualsilvestri14@gmail.com>
2022-06-18 23:54:52 -0300

    p

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ for(let i = 0 ; i < img.length ; i++){
 let video = document.querySelectorAll(".videos-entregas");
 let video2 = document.querySelectorAll(".videos");
 for(let i = 0 ; i < img.length ; i++){
-     img2[i].addEventListener("click", function(){
-     img2[i].classList.toggle("activar");
-     img[i].classList.toggle("activar2");
+     video2[i].addEventListener("click", function(){
+     video2[i].classList.toggle("activar");
+     video[i].classList.toggle("activar2");
      });
 }
