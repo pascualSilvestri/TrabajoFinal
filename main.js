7ae768c4da6c7cc7fe7d7f82862d115effb01c71
pascualSilvestri <pascualsilvestri14@gmail.com>
2022-06-18 21:49:38 -0300

    p

--- main.js
+++ main.js
@@ -3,11 +3,7 @@ let img2 = document.querySelectorAll(".img-entregas");
 for(let i = 0 ; i < img.length ; i++){
      img2[i].addEventListener("click", function(){
      img2[i].classList.toggle("activar");
+     img[i].classList.toggle("activar2");
      });
 }
 
-for(let i = 0 ; i < img.length ; i++){
-    img[i].addEventListener("click", function(){
-    img[i].classList.toggle("activar2");
-    });
-}
